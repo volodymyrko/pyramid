212ae939caf23ed8034c832af4a4bc2ff3c4c34d
Volodymyr Kovtun <vova-zms@yandex.ua>
2013-04-24 17:26:53 +0300

    add time counter

--- tracktime/tracktime/models.py
+++ tracktime/tracktime/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from sqlalchemy import (
     Column,
     Integer,
     Text,
+    DateTime
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,7 +15,8 @@ from sqlalchemy.orm import (
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension(),
+    expire_on_commit=False))
 Base = declarative_base()
 
 
@@ -26,3 +29,11 @@ class MyModel(Base):
     def __init__(self, name, value):
         self.name = name
         self.value = value
+
+
+class TrackTimeEntry(Base):
+    __tablename__ = 'tracktime'
+    id = Column(Integer, primary_key=True)
+    start_time = Column(DateTime(), default=datetime.now)
+    stop_time = Column(DateTime(), nullable=True)
+    msg = Column(Text)
--- tracktime/tracktime/views.py
+++ tracktime/tracktime/views.py
@@ -1,3 +1,6 @@
+import transaction
+from datetime import datetime
+
 from pyramid.response import Response
 from pyramid.view import view_config
 
@@ -6,8 +9,9 @@ from sqlalchemy.exc import DBAPIError
 from .models import (
     DBSession,
     MyModel,
+    TrackTimeEntry,
     )
-
+from .utils import time_diff_in_second
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
@@ -33,6 +37,78 @@ After you fix the problem, please restart the Pyramid application to
 try it again.
 """
 
+
 @view_config(route_name='test', renderer='templates/test.pt')
 def test_view(request):
     return {'one': 'one', 'project': 'tracktime'}
+
+
+@view_config(route_name='counter_status', renderer='json')
+def counter_status(request):
+    #import pdb; pdb.set_trace()
+    response = {
+        'status': 'new',
+        }
+    last = DBSession.query(TrackTimeEntry).order_by('-id').first()
+    if last and last.stop_time is None:
+        response['status'] = 'continue'
+        response['sec'] = time_diff_in_second(datetime.now(), last.start_time)
+        response['id'] = last.id
+    
+
+    return response
+
+#session = DBSession()
+
+@view_config(route_name='counter_start', renderer='json')
+def counter_start(request):
+    #import pdb; pdb.set_trace()
+    
+    last = DBSession.query(TrackTimeEntry).order_by('-id').first()
+    if last and last.stop_time is None:
+        response = {'status': 0}
+    else:
+        entry = TrackTimeEntry()
+        DBSession.add(entry)
+        transaction.commit()
+
+        response = {
+            'status': 1,
+            'id': entry.id
+            }
+
+    return response
+
+    
+@view_config(route_name='counter_stop', renderer='json')
+def counter_stop(request):
+    response = {
+        'status': 0,
+    }
+    pk = request.matchdict['pk']
+    entry = DBSession.query(TrackTimeEntry).get(pk)
+    if entry:
+        entry.stop_time = datetime.now()
+        DBSession.add(entry)
+        transaction.commit()
+        response['status'] = 1
+    return response
+
+    
+@view_config(route_name='counter_msg', renderer='json')
+def counter_msg(request):
+    response = {
+        'status': 0,
+    }
+    pk = request.matchdict['pk']
+    entry = DBSession.query(TrackTimeEntry).get(pk)
+    #import pdb; pdb.set_trace()
+    if entry:
+        msg = request.POST.get('data')
+        if msg:
+            entry.msg = msg
+            DBSession.add(entry)
+            transaction.commit()
+            response['status'] = 1
+    return response
+
